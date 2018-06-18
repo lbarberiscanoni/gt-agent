c9c153d2573a2e0cb04b0b9c42d33f297ea47752
lbarberiscanoni <lorenzo.barberis.canonico@gmail.com>
2018-06-17 17:12:03 -0700

    worked out some graphics for the flash crashes

--- graph.py
+++ graph.py
@@ -6,42 +6,63 @@ f = open("market.pkl", "rb")
 
 txt = pickle.load(f)
 
+# for i in range(len(txt)):
+
+# 	data = txt[i]["long"]
+# 	data2 = txt[i]["short"]
+
+# 	#center = (bins[:-1] + bins[1:]) / 2
+# 	#plt.bar(center, hist, align='center', width=width)
+# 	# plt.hist(data, 5, facecolor='blue', align="mid")
+# 	# plt.show()
+
+# 	bubbles = []
+# 	correction = []
+# 	growth = []
+
+# 	for x in data:
+# 		if x >= .7:
+# 			bubbles.append(x)
+# 		elif x <= .3:
+# 			correction.append(x)
+# 		else:
+# 			growth.append(x)
+
+# 	print(i + 1)
+# 	print("bubbles %", len(bubbles) / float(len(data)))
+# 	print("correction %", len(correction) / float(len(data)))
+# 	print("growth %", len(growth) / float(len(data)))
+# 	print("median:", np.median(data))
+# 	print("std:", np.std(data))
+# 	print("corr:", np.corrcoef(data, data2)[1, 0])
+# 	print("------------")
+
+# 	#plotting the values
+# 	x = [i for i in range(len(data))]
+# 	plt.scatter(data, data2)
+# 	plt.savefig("resource" + str(i + 1) + ".png")
+
 for i in range(len(txt)):
 
-	data = txt[i]["long"]
-	data2 = txt[i]["short"]
+	longData = txt[i]["long"]
+	shortData = txt[i]["short"]
 
-	#center = (bins[:-1] + bins[1:]) / 2
-	#plt.bar(center, hist, align='center', width=width)
-	# plt.hist(data, 5, facecolor='blue', align="mid")
-	# plt.show()
+	shortInterest = []
+	x = []
+	y = []
+	for j in range(len(longData)):
+		if longData[j] < .3:
+			_ = shortData[j] / float(1 - longData[j])
+			shortInterest.append(_)
 
-	bubbles = []
-	correction = []
-	growth = []
+			x.append(longData[j])
+			y.append(shortData[j])
 
-	for x in data:
-		if x >= .7:
-			bubbles.append(x)
-		elif x <= .3:
-			correction.append(x)
-		else:
-			growth.append(x)
+	print("resource", i + 1)
+	print("median", np.median(shortInterest))
+	print("mean", np.mean(shortInterest))
+	print("std", np.std(shortInterest))
+	print("corr", np.corrcoef(x, y)[1, 0])
+	print("---------")
 
-	print(i + 1)
-	print("bubbles")
-	print("%", len(bubbles) / float(len(data)))
-	print("correction")
-	print("%", len(correction) / float(len(data)))
-	print("growth")
-	print("%", len(growth) / float(len(data)))
-	print("mean:", np.mean(data))
-	print("std:", np.std(data))
-	print("corr:", np.corrcoef(data, data2)[1, 0])
-	print("------------")
-
-	#plotting the values
-	x = [i for i in range(len(data))]
-	plt.plot(x, data)
-	plt.plot(x, data2)
 
