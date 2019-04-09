93531265eab70c62a6badc3eac763d9265cf7608
lbarberiscanoni <lorenzo.barberis.canonico@gmail.com>
2019-04-09 17:52:07 -0400

    added an automatic runner

---
+++ flash-crashes_new/run.py
@@ -0,0 +1,16 @@
+import subprocess
+import itertools
+
+combos = [
+	"trpo", "vpg", "dqn", "trpo vpg", "vpg dqn", "dqn", "trpo", "trpo vpg dqn"
+]
+
+resources = [1, 5, 10]
+# thresholds = [
+# 	[.7, .3], [.6, .4], [.8, .2]
+# ]
+
+for resource in resources:
+	for combo in combos:
+		print(resource, combo)
+		subprocess.call("python factory.py --population=12 --resources=" + str(resource) + " --agent " + combo + " --ceiling=.7 --floor=.3", shell=True)
